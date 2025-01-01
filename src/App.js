d586313a6a3b1a4c641d8c553fdc3c7a5c3a0dc8
aria namdari <ariyanamdari00711@outlook.com>
2025-01-01 22:15:18 +0330

    last-b-1-diploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+// src/App.js
+
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import StoryPage from "./pages/StoryPage";
 import About from "./pages/About"; // وارد کردن صفحه About
